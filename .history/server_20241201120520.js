1ffee15d2b9be493f61f6e5c8d3a395e76422bba
sangeeta <sanjuother107@gmail.com>
2024-12-01 12:08:17 +0530

    otp-backend-code

---
+++ .history/server_20241201120520.js
@@ -0,0 +1,26 @@
+//mongodb
+
+require('./config/db')
+
+const app = require('express')()
+const port = process.env.PORT || 3000 
+
+// cors
+const cors = require('cors');
+app.use(cors());
+
+
+const UserRouter = require('./api/User')
+
+//For accepting post form data 
+
+const bodyParser = require('express').json 
+app.use(bodyParser())
+
+app.use('/user', UserRouter)
+
+app.use("/", UserRouter) 
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
